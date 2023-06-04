f9d3379b5244025c019ec1e2635e06d84dbb4807
MaximRogov <113343863+MaximRogov@users.noreply.github.com>
2023-06-04 20:20:23 +0300

    Create 26.java

---
+++ 2SEM/JAVA/26/26.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        int maxLength = 0;
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                String subStr = str.substring(i, j);
+                if (isPalindrome(subStr) && subStr.length() > maxLength) {
+                    maxLength = subStr.length();
+                }
+            }
+        }
+        System.out.println(maxLength);
+    }
+
+    private static boolean isPalindrome(String str) {
+        int i = 0;
+        int j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
