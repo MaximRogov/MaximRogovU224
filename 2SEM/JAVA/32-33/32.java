e0322929cb9ba590b0cb6c0d81b90b0567ded960
MaximRogov <113343863+MaximRogov@users.noreply.github.com>
2023-06-04 20:52:12 +0300

    Create 32.java

---
+++ 2SEM/JAVA/32-33/32.java
@@ -0,0 +1,38 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        long num = sc.nextLong();
+        game(num);
+    }
+    public static void game(long n) {
+        int Tanya = 0;
+        while (n != 1) {
+            if (n % 2 == 0) {
+                Tanya++;
+                n--;
+                if (n == 1) {
+                    break;
+                } else {
+                    n--;
+                }
+
+            } else {
+                Tanya++;
+                n--;
+                if (n % 2 == 0) {
+                    n /= 2;
+                    if (n == 1) {
+                        break;
+                    }
+
+                }
+            }
+
+        }
+        System.out.println(Tanya);
+    }
+}
